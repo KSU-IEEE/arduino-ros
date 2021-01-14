9ffe615606912bee834a1d8f30a63e7a93a2f949
Trevor Stanca <tstanca@students.kennesaw.edu>
2021-01-14 11:00:40 -0500

    updated chatter to work with the test behaviors, launches from the statemachine directory

--- test/chatter.cpp
+++ test/chatter.cpp
@@ -5,15 +5,34 @@
 
 ros::NodeHandle nh;
 
+// publisher messages
 std_msgs::String str_msg;
+std_msgs::String my_msg;
+
+// define publishers
 ros::Publisher chatter("chatter", &str_msg);
+ros::Publisher arduino_ros("/arduino/message", &my_msg);
+
+// callback functions 
+void tester_cb(const std_msgs::String& str) {
+  my_msg.data = str.data;
+
+  arduino_ros.publish( &my_msg );
+}
+
+
+// define subscribers
+ros::Subscriber<std_msgs::String> tester_sub("tester1/message", &tester_cb);
 
 char hello[13] = "hello world!";
+
 
 void setup()
 {
   nh.initNode();
   nh.advertise(chatter);
+  nh.advertise(arduino_ros);
+  nh.subscribe(tester_sub);
 }
 
 void loop()
